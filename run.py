e7af0611f75fa5c535dbd951844cd3426c798bde
RochmatBasukiXD <103634965+RozhBasXYZ@users.noreply.github.com>
2022-06-08 11:55:56 +0700

    Update run.py

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ try:ngr = requests.get("http://ip-api.com/json/").json();bas = ngr["country"]
 except:bas = "Indonesia"
 if __name__ == "__main__":
 	if "Indonesia" == bas:
-		__import__("DUMP").menu()
+		__import__("DUMP").main()
 	else:exit("sorry this script is only for Indonesian people")
