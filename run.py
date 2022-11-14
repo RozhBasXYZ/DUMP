911de230e9d65c743869b5130450bd4d8ac64511
RochmatBasukiXD <103634965+RozhBasXYZ@users.noreply.github.com>
2022-11-14 20:45:47 +0700

    Delete run.py

--- run.py
+++
@@ -1,8 +0,0 @@
-import os,requests
-try:ngr = requests.get("http://ip-api.com/json/").json();bas = ngr["country"]
-except:bas = "Indonesia"
-if __name__ == "__main__":
-	if "Indonesia" == bas:
-		os.system('git pull')
-		__import__("DUMP").main()
-	else:exit("sorry this script is only for Indonesian people")
